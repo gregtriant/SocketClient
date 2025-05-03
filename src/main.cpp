9e11a0914ad00d1d639fafa5fb15767d3f8c3d18
gregtriant <greg.triant@outlook.com>
2025-05-03 20:52:28 +0300

    initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,50 @@
+#include <Arduino.h>
+#include <arduino-timer.h>
 #include "SocketClient.h"
 #include "globals.h"
+#include "DHT.h"
 
-// const char* ssid     = "dobbielink"; // declared in globals.h
-// const char* password = "#########";  // declared in globals.h
-// const char* token = "xxxxxxxxxx";    // declared in globals.h
+#define VERSION 1.0
 
+#define LED_PIN 33
+#define LIGHT_SENSOR_PIN 35
+#define DHTPIN 32
+#define DHTTYPE DHT11
+
+typedef struct {
+  int lightSensorValue;        // Raw value from the light sensor (0-4095 for 12-bit ADC)
+  float lightSensorPercentage; // Percentage of light (0-100%)
+} LightSensorData;
+typedef struct {
+  float temperature; // Temperature in Celsius
+  float humidity;    // Humidity in percentage
+  float heatIndex;   // Heat index in Celsius
+} DHTData;
+
+DHT dht(DHTPIN, DHTTYPE);
 SocketClient testClient;
-String message = "hello";
-int count = 0;
-int led_state = 0;
-int slider = 0;
+auto send_data_timer = timer_create_default();
 
-void setLedState(int ledState)
-{
-  led_state = ledState;
-  digitalWrite(LED_BUILTIN, 1 - ledState);
-}
-void setSliderState(int sliderState)
-{
-  slider = sliderState;
-}
+// functions
+DHTData readDHT();
+LightSensorData readLightSensor();
 
 void sendStatus(JsonDoc &status)
 {
   status.clear();
-  status["message"] = message + String(count);
-  status["led"] = String(led_state);
-  status["slider"] = String(slider);
+  DHTData dhtData = readDHT();
+  LightSensorData lightSensorData = readLightSensor();
+  status["Temperature C"] = dhtData.temperature;
+  status["Humidity %"] = dhtData.humidity;
+  status["HeatIndex C"] = dhtData.heatIndex;
+  status["LightValue"] = lightSensorData.lightSensorValue;
+  status["Light %"] = lightSensorData.lightSensorPercentage;
 }
 
 void receivedCommand(JsonDoc &doc)
 {
-  // String stringData = "";
-  // serializeJson(doc, stringData);
-  // Serial.print("Command: ");
-  // Serial.println(stringData);
   if (strcmp(doc["data"], "count") == 0)
   {
-    count++;
     testClient.sendStatusWithSocket();
   }
 }
@@ -50,13 +57,18 @@ void entityChanged(JsonDoc &doc)
   // Serial.println(stringData);
   if (strcmp(doc["entity"], "led") == 0)
   {
-    setLedState(atoi(doc["value"]));
+    // setLedState(atoi(doc["value"]));
   }
   else if (strcmp(doc["entity"], "slider") == 0)
   {
-    setSliderState(doc["value"]);
+    // setSliderState(doc["value"]);
     testClient.sendStatusWithSocket();
   }
+}
+
+bool sendStatusNow(void* context) {
+  testClient.sendStatusWithSocket(true);
+  return true; // return true to keep the timer running
 }
 
 void setup()
@@ -65,26 +77,12 @@ void setup()
   Serial.begin(115200);
   delay(1000);
 
-  // Connect to the network
-  WiFi.begin(ssid, password);
-  Serial.print("\nConnecting to ");
-  Serial.print(ssid);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.print('.');
-  }
-  Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  setLedState(led_state);
+  testClient.initWifi();
+  testClient.setLedPin(LED_PIN); // set the led pin to blink when connected
 
-  // test client
-  // testClient.setSocketHost("api.sensordata.space", 80, false);  //192.168.0.56
-  testClient.setAppAndVersion("SocketClient", 0.12);
+  testClient.setAppAndVersion("Solar", VERSION);
   testClient.setToken(token);
-  //-testClient.setDeviceType("ESP8266");
   testClient.setSendStatusFunction(sendStatus);
   testClient.setReceivedCommandFunction(receivedCommand);
   testClient.setEntityChangedFunction(entityChanged);
@@ -97,19 +95,82 @@ void setup()
 
     // send connected notification
     doc.clear();
-    // you can add some extra oprions in the doc in you have pushover!
-    // doc["url"] = ""; //optional
-    // doc["url_title"] = ""; //optional
-    // doc["html"] = ""; //optional
-    // doc["priority"] = ""; //optional
-    // doc["sound"] = "cosmic"; //optional
-    // doc["timestamp"] = ""; //optional
     testClient.sendNotification("Connected!");
+    testClient.sendStatusWithSocket(true);
   });
   testClient.init("api.sensordata.space", 443, true); // if you dont want ssl use .init and change the port.
-  // testClient.init("192.168.1.81", 3030, false);
+  
+  
+  dht.begin();
+  send_data_timer.every(5*60*1000, sendStatusNow, nullptr);
 }
+
+
 void loop()
 {
   testClient.loop();
+  send_data_timer.tick();
 }
+
+
+LightSensorData readLightSensor()
+{
+  delay(2000);
+  // read the light sensor value
+  int lightValue = analogRead(LIGHT_SENSOR_PIN);
+  float percentage = (lightValue / 4095.0) * 100; // Assuming a 12-bit ADC (0-4095)
+  // send the light value to the server
+  Serial.print("Light: ");
+  Serial.println(lightValue);
+  Serial.print("Light %: ");
+  Serial.println(percentage);
+
+  LightSensorData lightSensorData = {
+    .lightSensorValue = lightValue,
+    .lightSensorPercentage = percentage,
+  };
+
+  return lightSensorData;
+}
+
+DHTData readDHT() {
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+
+  // Check if any reads failed and exit early (to try again).
+  DHTData dhtData = {
+    .temperature = 0,
+    .humidity = 0,
+    .heatIndex = 0,
+  };
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return dhtData;
+  }
+
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+
+  dhtData = {
+    .temperature = t,
+    .humidity = h,
+    .heatIndex = hic,
+  };
+  
+  return dhtData;
+}
+
