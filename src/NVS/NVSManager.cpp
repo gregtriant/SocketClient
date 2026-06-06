13baa936d5d2020c1894aaabaeea075149ce64c9
gregtriant <greg.triant@outlook.com>
2026-06-06 14:09:13 +0300

    refactor(nvm): make NVSManager general-purpose with typed get/set
    Replace WiFi-specific _wifi_preferences with general-purpose _prefs member.
    Add typed getUInt/putUInt and getString/putString methods with namespace
    parameter for storing arbitrary data in NVS. Preserve backward-compatible
    saveWifiCredentials/getWifiCredentials methods for WiFi module.

--- src/NVS/NVSManager.cpp
+++ src/NVS/NVSManager.cpp
@@ -1,30 +1,41 @@
 #include "NVSManager.h"
 
-
 NVSManager::NVSManager() {}
 
 NVSManager::~NVSManager() {}
 
-void NVSManager::saveWifiCredentials(String ssid, String password)
-{
-    _wifi_preferences.begin(NVS_WIFI_NAMESPACE, RW_MODE);
-    _wifi_preferences.putString(NVS_WIFI_SSID_TOKEN, ssid);
-    _wifi_preferences.putString(NVS_WIFI_PASSWORD_TOKEN, password);
-    _wifi_preferences.end();
+uint32_t NVSManager::getUInt(const char* ns, const char* key, uint32_t defaultValue) {
+    _prefs.begin(ns, RO_MODE);
+    uint32_t val = _prefs.getUInt(key, defaultValue);
+    _prefs.end();
+    return val;
 }
 
+void NVSManager::putUInt(const char* ns, const char* key, uint32_t value) {
+    _prefs.begin(ns, RW_MODE);
+    _prefs.putUInt(key, value);
+    _prefs.end();
+}
 
-void NVSManager::getWifiCredentials(String &ssid, String &password)
-{
-    _wifi_preferences.begin(NVS_WIFI_NAMESPACE, RW_MODE);
+String NVSManager::getString(const char* ns, const char* key, const String& defaultValue) {
+    _prefs.begin(ns, RO_MODE);
+    String val = _prefs.getString(key, defaultValue);
+    _prefs.end();
+    return val;
+}
 
-    if (false) {
-        Serial.println("Initializing NVS for the first time.");
-        _wifi_preferences.putString(NVS_WIFI_SSID_TOKEN, "");
-        _wifi_preferences.putString(NVS_WIFI_PASSWORD_TOKEN, "");
-    }
+void NVSManager::putString(const char* ns, const char* key, const String& value) {
+    _prefs.begin(ns, RW_MODE);
+    _prefs.putString(key, value.c_str());
+    _prefs.end();
+}
 
-    ssid = _wifi_preferences.getString(NVS_WIFI_SSID_TOKEN);
-    password = _wifi_preferences.getString(NVS_WIFI_PASSWORD_TOKEN);
-    _wifi_preferences.end();
+void NVSManager::saveWifiCredentials(String ssid, String password) {
+    putString(NVS_WIFI_NAMESPACE, NVS_WIFI_SSID_TOKEN, ssid);
+    putString(NVS_WIFI_NAMESPACE, NVS_WIFI_PASSWORD_TOKEN, password);
+}
+
+void NVSManager::getWifiCredentials(String& ssid, String& password) {
+    ssid     = getString(NVS_WIFI_NAMESPACE, NVS_WIFI_SSID_TOKEN, "");
+    password = getString(NVS_WIFI_NAMESPACE, NVS_WIFI_PASSWORD_TOKEN, "");
 }
